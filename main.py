916e0b25bbf7b381ba080f1832916d34f5f08be4
Ксения Тапок <soulssfg@gmail.com>
2025-11-17 19:57:18 +0300

    comment
    its not finally yet...

--- main.py
+++ main.py
@@ -1,32 +1,57 @@
 import tkinter as tk
+from clicker import *
+from buttons import *
+
+current_player = "X"
+
+# Функция для заполнения клеток поля 
+def on_click(index):
+    global current_player
+    if buttons[index]["text"] == "":
+        buttons[index]["text"] = current_player
+        if current_player == "X":
+            current_player = "O"  
+        else:
+            current_player = "X"
+
+# Функция для очистки поля
+def clean_space():
+    for i in range(9):
+        buttons[i]["text"] = ""
+
 
 # Настройка окна
 root = tk.Tk()
 root.title("Tic-Tac-Toe")
-root.iconbitmap("resources/icon/icon.ico")
+root.iconbitmap("resources/icon/2.ico")
 root.resizable(0, 0)
-# Формирование поля для кнопок
+
+# Формирование поля
 buttons = []
 for i in range (9):
     button = tk.Button(
         root,
         text="",
-        font=("Arial", 30),
+        font=("Arial", 16),
         width=5,
         height=2,
+        command=lambda idx=i: on_click(idx)
     )
     button.grid(row=i//3, column=i%3)
     buttons.append(button)
 
+# Кнопочки меню
+menu = []
+clearButton = tk.Button(
+    root,
+    text="Очистить\nполе",
+    font=("Arial", 8),
+    width=10,
+    height=2,
+    command=lambda: clean_space()
+)
+clearButton.grid(row=3, column=1)
+menu.append(clearButton)
 
-
-
-
-
-
-
-
-
-
-# Запуск главного цикла программы
+# Запуск игрушки
 root.mainloop()
