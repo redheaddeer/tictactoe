5b7984cc61c41845768d5631daa20d42079e32fd
Ксения Тапок <soulssfg@gmail.com>
2025-11-18 21:52:30 +0300

    its over

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import tkinter as tk
-from clicker import *
-from buttons import *
 
 current_player = "X"
 
@@ -13,12 +11,76 @@ def on_click(index):
             current_player = "O"  
         else:
             current_player = "X"
+        check_streak()
+    
 
 # Функция для очистки поля
 def clean_space():
     for i in range(9):
         buttons[i]["text"] = ""
+        buttons[i]["background"] = ("#ffffff")
+        buttons[i]["state"]=("normal")
 
+def check_streak():
+    streak = False
+    if (buttons[0]["text"] == buttons[1]["text"] == buttons[2]["text"]
+        and buttons[0]["text"] != ""):
+        streak = True
+        buttons[0]["background"] = ("#55ff00")
+        buttons[1]["background"] = ("#55ff00")
+        buttons[2]["background"] = ("#55ff00")
+    else:
+        if (buttons[3]["text"] == buttons[4]["text"] == buttons[5]["text"]
+            and buttons[3]["text"] != ""):
+            streak = True
+            buttons[3]["background"] = ("#55ff00")
+            buttons[4]["background"] = ("#55ff00")
+            buttons[5]["background"] = ("#55ff00")
+        else:
+            if (buttons[6]["text"] == buttons[7]["text"] == buttons[8]["text"]
+                and buttons[6]["text"] != ""):
+                streak = True
+                buttons[6]["background"] = ("#55ff00")
+                buttons[7]["background"] = ("#55ff00")
+                buttons[8]["background"] = ("#55ff00")
+            else:
+                if (buttons[0]["text"] == buttons[3]["text"] == buttons[6]["text"]
+                    and buttons[0]["text"] != ""):
+                    streak = True
+                    buttons[0]["background"] = ("#55ff00")
+                    buttons[3]["background"] = ("#55ff00")
+                    buttons[6]["background"] = ("#55ff00")
+                else:
+                    if (buttons[1]["text"] == buttons[4]["text"] == buttons[7]["text"]
+                        and buttons[1]["text"] != ""):
+                        streak = True
+                        buttons[1]["background"] = ("#55ff00")
+                        buttons[4]["background"] = ("#55ff00")
+                        buttons[7]["background"] = ("#55ff00")
+                    else:
+                        if (buttons[2]["text"] == buttons[5]["text"] == buttons[8]["text"]
+                            and buttons[2]["text"] != ""):
+                            streak = True
+                            buttons[2]["background"] = ("#55ff00")
+                            buttons[5]["background"] = ("#55ff00")
+                            buttons[8]["background"] = ("#55ff00")
+                        else:
+                            if (buttons[0]["text"] == buttons[4]["text"] == buttons[8]["text"]
+                                and buttons[0]["text"] != ""):
+                                streak = True
+                                buttons[0]["background"] = ("#55ff00")
+                                buttons[4]["background"] = ("#55ff00")
+                                buttons[8]["background"] = ("#55ff00")
+                            else:
+                                if (buttons[2]["text"] == buttons[4]["text"] == buttons[6]["text"]
+                                    and buttons[2]["text"] != ""):
+                                    streak = True
+                                    buttons[2]["background"] = ("#55ff00")
+                                    buttons[4]["background"] = ("#55ff00")
+                                    buttons[6]["background"] = ("#55ff00")
+    if streak:
+        for i in range(9):
+            buttons[i]["state"]=("disabled")
 
 # Настройка окна
 root = tk.Tk()
@@ -35,6 +97,7 @@ for i in range (9):
         font=("Arial", 16),
         width=5,
         height=2,
+        background=("#ffffff"),
         command=lambda idx=i: on_click(idx)
     )
     button.grid(row=i//3, column=i%3)
