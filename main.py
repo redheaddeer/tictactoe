f9546d8af04f6db8daa68f0769b5b5578e21e45d
Ксения Тапок <soulssfg@gmail.com>
2025-11-18 22:12:08 +0300

    бьютифул я считаю

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import tkinter as tk
 
 current_player = "X"
 
+# Настройка окна
+root = tk.Tk()
+root.title("Tic-Tac-Toe")
+root.iconbitmap("resources/icon/2.ico")
+root.resizable(0, 0) # нам не надо, чтобы у окна была возможность расползаться по экрану
+
 # Функция для заполнения клеток поля 
 def on_click(index):
     global current_player
-    if buttons[index]["text"] == "":
+    if buttons[index]["text"] == "": # мы можем изменить значение кнопки только если она пустая
         buttons[index]["text"] = current_player
         if current_player == "X":
-            current_player = "O"  
+            current_player = "O"
         else:
             current_player = "X"
         check_streak()
-    
 
 # Функция для очистки поля
 def clean_space():
@@ -20,13 +25,15 @@ def clean_space():
         buttons[i]["text"] = ""
         buttons[i]["background"] = ("#ffffff")
         buttons[i]["state"]=("normal")
+    global current_player
+    current_player = "X" # крестики всегда начинают первые, поэтому сбрасываем current_player также до "X"
 
 def check_streak():
-    streak = False
+    streak = False # сначала мы не знаем, есть ли стрики на карте 
     if (buttons[0]["text"] == buttons[1]["text"] == buttons[2]["text"]
-        and buttons[0]["text"] != ""):
+        and buttons[0]["text"] != ""): # если хотя бы одно поле не пустое, и при этом поля равны, значит они все равны X или O
         streak = True
-        buttons[0]["background"] = ("#55ff00")
+        buttons[0]["background"] = ("#55ff00") # красим фон в приятный зелёный цвет, приятный же, ну?
         buttons[1]["background"] = ("#55ff00")
         buttons[2]["background"] = ("#55ff00")
     else:
@@ -80,13 +87,7 @@ def check_streak():
                                     buttons[6]["background"] = ("#55ff00")
     if streak:
         for i in range(9):
-            buttons[i]["state"]=("disabled")
-
-# Настройка окна
-root = tk.Tk()
-root.title("Tic-Tac-Toe")
-root.iconbitmap("resources/icon/2.ico")
-root.resizable(0, 0)
+            buttons[i]["state"]=("disabled") # и запрещаем дальнейшие изменения игрового поля, ибо game over 
 
 # Формирование поля
 buttons = []
